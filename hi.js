903b94f9d6ef4bd43ded7755f6583732fea930d9
juyeon <w2316@-emirim.hs.kr>
2023-12-29 21:52:28 +0900

    나이 계산하기

--- hi.js
+++
@@ -1,5 +0,0 @@
-var name = prompt("이름을 입력하세요");
-document.write("<b><big>"+ name +"</big></b>님, 환영합니다." );
-var age= prompt("나이를 입력하세요");  //prompt() : 사용자 입력값 받기
-document.write(age); //웹 브라우저 화면에 출력하기
-alert("환영합니다!"); //알림 창 출력
